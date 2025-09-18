8a420e461c2952998c427a7adfab48294e9005f4
croasoant <159121944+croasoant@users.noreply.github.com>
2025-09-18 08:57:41 +0300

    very good boy

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,7 +9,7 @@ exports.register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const existing = await User.findOne({ email });
-    if (existing) return res.status(400).json({ message: 'Email already ' });
+    if (existing) return res.status(400).json({ message: 'Email already used' });
     const user = await User.create({ name, email, password });
     const token = signToken(user);
     res.status(201).json({ token, user: { id: user._id, name: user.name, email: user.email, role: user.role }});
